3acf3ec9a5c3692593bc034ebd5492612bce0e9f
Paweł Papkiewicz <papkie12@gmail.com>
2017-11-28 23:26:21 +0100

    Few fixes, but still not working

--- fs/api_pcd8544.js
+++ fs/api_pcd8544.js
@@ -1,10 +1,10 @@
 let PCD8544 = {
-  create: ffi('void *mgos_PCD8544_create(char SCLK, char DIN, char DC, char RST)'),
-  begin: ffi('void mgos_PCD8544_begin(unsigned char contrast, unsigned char bias)'),
-  command: ffi('void mgos_PCD8544_command(unsigned char c)'),
-  data: ffi('void mgos_PCD8544_data(unsigned char c)'),
-  setContrast: ffi('void mgos_PCD8544_setContrast(unsigned char val)'),
-  clearDisplay: ffi('void mgos_PCD8544_clearDisplay()'),
-  display: ffi('void mgos_PCD8544_display()'),
-  drawPixel: ffi('void mgos_PCD8544_drawPixel(short x, short y, unsigned short color)'),
+  create: ffi('void *mgos_PCD8544_create(char SCLK, char DIN, char DC, char CS, char RST)'),
+  begin: ffi('void mgos_PCD8544_begin(void* ctx, unsigned char contrast, unsigned char bias)'),
+  command: ffi('void mgos_PCD8544_command(void* ctx, unsigned char c)'),
+  data: ffi('void mgos_PCD8544_data(void* ctx, unsigned char c)'),
+  setContrast: ffi('void mgos_PCD8544_setContrast(void* ctx, unsigned char val)'),
+  clearDisplay: ffi('void mgos_PCD8544_clearDisplay(void* ctx)'),
+  display: ffi('void mgos_PCD8544_display(void* ctx)'),
+  drawPixel: ffi('void mgos_PCD8544_drawPixel(void* ctx, short x, short y, unsigned short color)'),
 };
--- fs/init.js
+++ fs/init.js
@@ -1,14 +1,31 @@
 load('api_config.js');
 load('api_timer.js');
+load('api_gpio.js');
 load('api_pcd8544.js');
 
-let SCLK = 1,
-  DIN = 2,
-  DC = 3,
-  RST = 4;
+let SCLK = 5,
+  DIN = 4,
+  DC = 14,
+  RST = 2,
+  CS = 12;
 
-const lcd = PCD8544.init(SCLK, DIN, DC, RST)
-
-Timer.set(1000 /* 1 sec */ , true /* repeat */ , function () {
-  print("Alive")
+const lcd = PCD8544.init(SCLK, DIN, DC, CS, RST);
+PCD8544.display(lcd);
+let time = 0;
+print("Hello");
+Timer.set(10, true, function () {
+  PCD8544.setContrast(lcd, time++);
+  PCD8544.drawPixel(lcd, 1, 1, 0);
+  PCD8544.drawPixel(lcd, 2, 2, 255);
+  PCD8544.drawPixel(lcd, 3, 3, 0);
+  PCD8544.drawPixel(lcd, 4, 4, 255);
+  PCD8544.drawPixel(lcd, 5, 5, 0);
+  PCD8544.drawPixel(lcd, 6, 6, 255);
+  PCD8544.drawPixel(lcd, 7, 7, 0);
+  PCD8544.drawPixel(lcd, 8, 8, 255);
+  PCD8544.drawPixel(lcd, 9, 9, 0);
+  PCD8544.drawPixel(lcd, 10, 10, 255);
+  PCD8544.drawPixel(lcd, 11, 11, 0);
+  PCD8544.drawPixel(lcd, 12, 12, 255);
+  PCD8544.display(lcd); 
 }, null);
